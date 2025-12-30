4983023c8fcdb532211db9291345af424ac52feb
Pratyush Manivansh <pratyushmanivansh711@gmail.com>
2025-12-30 12:36:37 +0530

    Adding in the git

---
+++ src/cwh_13_conditionals.java
@@ -0,0 +1,12 @@
+public class cwh_13_conditionals {
+
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (age > 0) {
+            System.out.println("The number is positive");
+        } else {
+            System.out.println("The number is negative or zero");
+        }
+    }
+}
