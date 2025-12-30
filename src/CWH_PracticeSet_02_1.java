12530ce2d50ccf21ce4f94c232299f2af9a24303
Pratyush Manivansh <pratyushmanivansh711@gmail.com>
2025-12-30 09:56:29 +0530

    Adding in the git

--- src/CWH_PracticeSet_02_1.java
+++ src/CWH_PracticeSet_02_1.java
@@ -3,7 +3,7 @@ public class CWH_PracticeSet_02_1 {
     public static void main(String[] args) {
     // Result of the Expression
         System.out.println("Result Of The Expression: ");
-        float a = (7/4*9/2);
+        float a = (7/4.0f*9/2.0f);
 
         System.out.println(a);
     }
