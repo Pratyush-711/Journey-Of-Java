709dc2bb974c5f76ebf39427e6ea177535b6d738
Pratyush-711 <151891936+Pratyush-711@users.noreply.github.com>
2025-12-31 12:08:17 +0530

    Add files via upload

---
+++ cwh_15_elseif.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class cwh_15_elseif {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age: ");
+        int age = sc.nextInt();
+
+        if (age > 18) {
+            System.out.println("You are eligible for vote.");
+        }
+        else if (age < 18) {
+            System.out.println("You are not eligible for vote.");
+        }
+        else {
+            System.out.println("You are ready to vote.");
+        }
+    }
+}
