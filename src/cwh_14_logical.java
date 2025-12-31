8cf3d39df70aec350bd1830c1f57e364af7a2842
Pratyush Manivansh <pratyushmanivansh711@gmail.com>
2025-12-31 10:57:25 +0530

    Adding in the git

---
+++ src/cwh_14_logical.java
@@ -0,0 +1,24 @@
+public class cwh_14_logical {
+
+    public static void main(String[] args) {
+        System.out.println("For Logical AND...");
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+        if (a && b && c) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        System.out.println("For Logical OR...");
+        boolean a1 = true;
+        boolean b1 = false;
+        boolean c1 = true;
+        if (a1 || b1 || c1) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
