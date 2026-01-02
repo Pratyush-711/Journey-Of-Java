7b316ad291da251296d1355e577f837cbd9d1c55
Pratyush Manivansh <pratyushmanivansh711@gmail.com>
2026-01-02 14:48:55 +0530

    Adding in the git

---
+++ src/cwh_18_ch4_do_while.java
@@ -0,0 +1,12 @@
+public class cwh_18_ch4_do_while {
+
+    public static void main(String[] args) {
+        // Quick Quiz
+        System.out.println("Print First n natural numbers: ");
+        int num = 1;
+        do {
+            System.out.println(num);
+            num++;
+        } while (num <= 100);
+    }
+}
