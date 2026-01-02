55b8ce1e099b14b305a4dddac2c468c53baedb15
Pratyush Manivansh <pratyushmanivansh711@gmail.com>
2026-01-02 12:07:44 +0530

    Adding in the git

---
+++ src/cwh_17_ch5_loops.java
@@ -0,0 +1,19 @@
+public class cwh_17_ch5_loops {
+
+    public static void main(String[] args) {
+        // While Loop
+        System.out.println("While Loop");
+        int num = 1;
+        while (num<=10) {
+            System.out.println(num);
+            num++;
+        }
+        // Quick Quiz
+        System.out.println("Print natural numbers from 100 to 200");
+        int n = 100;
+        while (n<=200) {
+            System.out.println(n);
+            n++;
+        }
+    }
+}
