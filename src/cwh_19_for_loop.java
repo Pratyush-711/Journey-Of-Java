e4228d50206f023c7038f520df520bb677c1acc2
Pratyush Manivansh <pratyushmanivansh711@gmail.com>
2026-01-03 11:20:58 +0530

    Adding in the git

---
+++ src/cwh_19_for_loop.java
@@ -0,0 +1,10 @@
+public class cwh_19_for_loop {
+
+    public static void main(String[] args) {
+
+        System.out.println("For Loop");
+        for (int i = 0; i<=10; i++) {
+            System.out.println(i);
+        }
+    }
+}
