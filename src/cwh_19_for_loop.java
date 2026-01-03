b619a67e6319b32e30d2911b48ef8098bbd9e4e9
Pratyush Manivansh <pratyushmanivansh711@gmail.com>
2026-01-03 12:03:27 +0530

    Adding in the git

--- src/cwh_19_for_loop.java
+++ src/cwh_19_for_loop.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
 public class cwh_19_for_loop {
 
     public static void main(String[] args) {
 
         System.out.println("For Loop");
-        for (int i = 0; i<=10; i++) {
+        System.out.println("Print n natural numbers in sequential form: ");
+        for (int i = 1; i<=10; i++) {
+            System.out.println(i);
+        }
+
+        // Quick Quiz
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Print n odd numbers");
+        System.out.println("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        System.out.println("First " + n + " odd numbers:");
+        for (int i = 1; i <= n; i++) {
+            System.out.println(2 * i - 1);
+        }
+        sc.close();
+
+        // Quick Quiz
+        System.out.println("Print n natural numbers in reverse order: ");
+        for (int i = 10; i>=1; i--) {
             System.out.println(i);
         }
     }
