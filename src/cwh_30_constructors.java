27e9feb7779dffe85b29a401c7481a864ddb6d9a
Pratyush Manivansh <pratyushmanivansh711@gmail.com>
2026-01-13 11:28:37 +0530

    Adding in the git

--- src/cwh_30_constructors.java
+++ src/cwh_30_constructors.java
@@ -1,36 +1,53 @@
+// Quick Quiz
 class MyMainEmployee {
     private int id;
     private String name;
+    private double salary;
 
-//    public MyMainEmployee() {
-//        id = 45;
-//        name = "Your-Name-Here";
-//    }
+
+    // Constructor 1: Name & ID (salary defaults to 10000)
     public MyMainEmployee(String myName, int myId) {
-        id = myId;
         name = myName;
+        id = myId;
+        salary = 10000;
     }
+
+    // Constructor 2: Name, ID & Salary (overloaded constructor)
+    public MyMainEmployee(String myName, int myId, double mySalary) {
+        name = myName;
+        id = myId;
+        salary = mySalary;
+    }
+
     public String getName() {
         return name;
     }
-    public void setName(String n) {
-        this.name = n;
-    }
+
     public int getId() {
         return id;
     }
-    public void setId(int i) {
-        this.id = i;
+    public double getSalary() {
+        return salary;
     }
 }
 
 public class cwh_30_constructors {
 
     public static void main(String[] args) {
-        MyMainEmployee emp = new MyMainEmployee("Pratyush Manivansh", 12);
-        // emp.setName("Code With Harry");
-        // emp.setId(100);
-        System.out.println(emp.getName());
-        System.out.println(emp.getId());
+        // Use default salary = 10000
+        MyMainEmployee emp1 = new MyMainEmployee("Pratyush Manivansh", 113);
+
+        // Use custom salary
+        MyMainEmployee emp2 = new MyMainEmployee("Code With Harry", 101, 25000);
+
+        System.out.println(emp1.getName());
+        System.out.println(emp1.getId());
+        System.out.println(emp1.getSalary());
+
+        System.out.println();
+
+        System.out.println(emp2.getName());
+        System.out.println(emp2.getId());
+        System.out.println(emp2.getSalary());
     }
 }
